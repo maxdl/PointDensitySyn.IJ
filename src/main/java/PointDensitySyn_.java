145d39226418e9f555bb82c22d791a3098e70428
Max Larsson <max.larsson@liu.se>
2018-06-13 14:39:56 +0200

    Bumped version to 1.3.3.

--- src/main/java/PointDensitySyn_.java
+++ src/main/java/PointDensitySyn_.java
@@ -26,10 +26,10 @@ import ij.measure.*;
 interface VersionPDS {
     String title = "PointDensitySyn";
     String author = "Max Larsson";
-    String version = "1.3.2";
+    String version = "1.3.3";
     String year = "2018";
-    String month = "May";
-    String day = "31";
+    String month = "June";
+    String day = "13";
     String email = "max.larsson@liu.se";
     String homepage = "http://www.liu.se/medfak/forskning/larsson-max/software";
 }    
